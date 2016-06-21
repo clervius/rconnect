78b490b47ce8a810e5f077833e13e77c1b3a74cf
Joram Clervius <clervius@gmail.com>
2016-06-21 01:56:35 -0400

    changed routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,49 @@
+var mongoose = require('mongoose');
 
-
-module.exports = function(app){
-	var db = mongoose.connection;
-
+module.exports = function(app, taxpayer, db){
+	
+	var	taxpayer = mongoose.model('taxpayer');
 	//routes
 	app.get('*', function(req, res){
-		res.render('index');
-		//console.log(taxpayers)
+		taxpayer.find({}).exec(function(err, taxpayers){
+			if(err){
+				res.render('index');
+				console.log('did not get tpayers');
+			}else{
+				res.render('index', { taxpayers : taxpayers});
+				console.log(taxpayers);
+			}
+		});
+
+		/*
+		res.render('index', function(){
+			taxpayer.find({}).exec(function(err, taxpayers){
+				if(err){
+					console.log('did not find taxpayers');
+				}else{
+					taxpayers: taxpayers
+					console.log('things look ok');
+				console.log(taxpayers[1])
+				}
+			});
+		});
+
+
+
+		taxpayer.find({}).exec(function(err, taxpayers){
+			if(err){
+				console.log('did not find taxpayers');
+			}else{
+				res.render('index', {
+					taxpayers: taxpayers
+				});
+				console.log('things look ok');
+				console.log(taxpayers[0])
+			}
+		});*/
+
+		
+		
 	})
 
 }
