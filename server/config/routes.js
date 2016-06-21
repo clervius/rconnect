f391cb9c9c67874ee9d0d2ef26b629f50aa3dbd1
Joram Clervius <clervius@gmail.com>
2016-06-21 18:42:59 -0400

    Added UI, looking good.

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,25 +4,26 @@ module.exports = function(app, taxpayer, db){
 	
 	var	taxpayer = mongoose.model('taxpayer');
 	//routes
+	app.get('/app/*', function(req, res){
+		taxpayer.find({}).exec(function(err, taxpayers){
+			if(err){
+				res.render('app');
+				console.log('did not get tpayers');
+			}else{
+				res.render('app', { taxpayers : taxpayers});
+				console.log(taxpayers[0]);
+			}
+		});
+	});
+
 	app.get('/partials/*', function(req, res){
-	    res.render('../../public/app/' + req.params[0]);
+	    res.render('../../public/app/' + req.params[0]); 
 	});
 
 	
 	app.get('*', function(req, res){
-		taxpayer.find({}).exec(function(err, taxpayers){
-			if(err){
-				res.render('index');
-				console.log('did not get tpayers');
-			}else{
-				res.render('index', { taxpayers : taxpayers});
-				console.log(taxpayers);
-			}
-		});
-
-		
-		
-		
-	})
+		// create a function that checks if you're logged in and forwards you to '/app/'
+		res.render('index');		
+	});
 
 }
