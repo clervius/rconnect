ad1dbfeda98da587bc57df3023705c9345813461
Joram Clervius <clervius@gmail.com>
2016-06-27 17:59:11 -0400

    first day back from Haiti.

--- server/api/taxpayer/index.js
+++ server/api/taxpayer/index.js
@@ -10,11 +10,5 @@ var router = express.Router();
 router.get('/app/', controller.newTaxpayers);
 router.get('/app/taxpayer/:id', controller.singleTaxpayer);
 
-router.get('/app/pending', controller.pendingTaxpayers);
-router.get('/app/pending/:tpId', controller.onePendingTaxpayer);
-
-router.get('/app/accepted', controller.acceptedTaxpayers);
-router.get('/app/accepted/:tpId', controller.oneAcceptedTaxpayers);
-
 
 module.exports = router;
